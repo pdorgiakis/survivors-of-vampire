42e70332923f6e3a9840fe4e1606f8070f180367
Panagiotis Dorgiakis <pdorgiakis@skroutz.gr>
2026-09-14 22:17:02 +0300

    Init GameActor class

---
+++ entities/GameActor.h
@@ -0,0 +1,26 @@
+#ifndef GAMEACTOR_H
+#define GAMEACTOR_H
+
+#include "../core/Base.h"
+#include "raylib.h"
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
+using namespace std;
+
+enum State { IDLE, WALK, JUMP, FALL, ATTACK, HURT, DEAD };
+struct Animation {
+  Texture texture;
+  int max_frames;
+};
+
+class GameActor : public Base {
+public:
+  GameActor(map<State, Animation> animations, bool main_player)
+      : animations(std::move(animations)), main_player(main_player) {};
+
+protected:
+  State state_{IDLE};
+  map<State, Animation> animations;
+  bool main_player{false};
+};
+#endif
